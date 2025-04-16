decb28d311c88198751f1912d6e852c4d0cd778f
menglanyx <3409143889@qq.com>
2025-04-16 15:19:44 +0800

    test2

--- script/enemy.gd
+++ script/enemy.gd
@@ -2,62 +2,51 @@ extends CharacterBody2D
 
 # GDScript file for enemy AI behavior in a 2D game
 
-var speed = 150
-var vision_range = 300
-var alertness = 0
-var max_alertness = 100
-var chase_threshold = 80
-var player = null
-var is_chasing = false
-var last_seen_position = Vector2.ZERO
+var speed : float = 100
+var vision_range : int = 300
+var alertness : int = 0
+var max_alertness : int = 100
+var chase_threshold : int = 80
+var player : CharacterBody2D = null
+var is_chasing : bool = false
+var last_seen_position : Vector2 = Vector2.ZERO
 
-var raycasts: Array = []
+var raycasts : Array = []
 
 func _ready():
-	player = get_node("/root/Main/player")
+	player = $"/root/Main/player"
 	print(player)
 	raycasts = [$Vision/RayCast1, $Vision/RayCast2, $Vision/RayCast3, $Vision/RayCast4, $Vision/RayCast5]
 
-func _physics_process(delta):
-	if is_player_in_vision():
-		increase_alertness()
+func _physics_process(_delta):
+	if player == null:
+		player = $"/root/Main/player"
+		return
+
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player < vision_range:
+		alertness += 1
+		if alertness >= chase_threshold:
+			is_chasing = true
+			last_seen_position = player.position
 	else:
-		reset_alertness()
+		alertness -= 1
+		if alertness <= 0:
+			is_chasing = false
 
 	if is_chasing:
-		chase_player(delta)
+		move_towards(last_seen_position)
 	else:
-		self.velocity = Vector2.ZERO
-
+				patrol()
+				# Check for obstacles using raycasts
+		
+func patrol():
+	# Example patrol logic: move left and right
+	var patrol_direction = Vector2.LEFT if int(position.x) % 200 < 100 else Vector2.RIGHT
+	velocity = patrol_direction * speed
 	move_and_slide()
-
-func is_player_in_vision() -> bool:
-	for raycast in raycasts:
-		if raycast.is_colliding():
-			var collider = raycast.get_collider()
-			if collider == player and !is_obstacle_between(raycast):
-				print(raycast.name)
-				return true
-	return false
-
-
-func is_obstacle_between(raycast: RayCast2D) -> bool:
-	return raycast.is_colliding() and raycast.get_collider() != player
-
-func increase_alertness():
-	alertness += 1
-	if alertness >= chase_threshold:
-		start_chasing()
-
-func reset_alertness():
-	alertness = max(0, alertness - 1)
-
-func start_chasing():
-	is_chasing = true
-	last_seen_position = player.position
-	print("追击")
-
-
-func chase_player(_delta):
-	var direction_to_player = (player.position - position).normalized()
-	self.velocity = direction_to_player * speed
+	
+func move_towards(target_position: Vector2):
+	var direction = (target_position - position).normalized()
+	velocity = direction * speed
+	move_and_slide()
