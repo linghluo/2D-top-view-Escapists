e37f068635de7aba03cbdd2f45bf5ee707121fc0
linghluo <2272853288@qq.com>
2025-04-16 13:43:26 +0800

    修改了player的八向移动，实现视角跟随，添加相机的基础功能

--- script/player.gd
+++ script/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@export var speed := 200.0
+@export var speed := 8000.0
 
-func _physics_process(_delta):
-	var input_vector = Vector2(
+func _physics_process(delta):
+	# 角色移动
+	var dir = Vector2(
 		Input.get_action_strength("p_right") - Input.get_action_strength("p_left"),
 		Input.get_action_strength("p_down") - Input.get_action_strength("p_up")
 	).normalized()
-
-	velocity = input_vector * speed
+	velocity = dir * speed * delta
 	move_and_slide()
+
+	# 角色朝向鼠标位置
+	look_at(get_global_mouse_position())
